c208beed0fe6baaf0e71329f9a6dc0f394f2aef5
Aikanysh Beksultanova <77458655+Aikanysh91@users.noreply.github.com>
2021-11-16 21:52:39 +0100

    Update and rename Task.java to MyTask.java
    My solution

--- src/main/java/com/example/demo/MyTask.java
+++ src/main/java/com/example/demo/MyTask.java
@@ -9,6 +9,11 @@ public class Task {
    */
 
   public static List<Person> getActivePlayersByScoreDesc(List<Person> people) {
+     people.stream()
+                    .filter(person -> person.isActive())
+                    .sorted(Comparator.comparing(Person::getScore))
+                    .collect(Collectors.toList());
+    
     throw new RuntimeException("Not Implemented!");
   }
 
@@ -17,6 +22,11 @@ public class Task {
    */
 
   public static List<Person> getActivePlayersByScoreDesc(List<Person> people, Group group) {
+    people.stream()
+                    .filter(person -> person.isActive())
+                    .filter(person -> person.getTeam().equals(group))
+                    .sorted(Comparator.comparing(Person::getScore))
+                    .collect(Collectors.toList());
     throw new RuntimeException("Not Implemented!");
   }
 
@@ -25,6 +35,10 @@ public class Task {
   Jeżeli ta liczba jest również równa, zwróć którąkolwiek z nich.
    */
   public static Group getGroupWithHighestScore(List<Person> people) {
+    people.stream()
+                    .sorted(Comparator.comparing(Person::getTeam))
+                    .max(Comparator.comparing(Person::getScore))
+                    .get();
     throw new RuntimeException("Not Implemented!");
   }
 
@@ -34,6 +48,9 @@ public class Task {
    */
 
   public static List<String> printPoints(List<Person> people) {
+     people.stream()
+                    .collect(Collectors.groupingBy(Person::getTeam));
+
     throw new RuntimeException("Not Implemented!");
   }
 }
